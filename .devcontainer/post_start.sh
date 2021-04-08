564e654c1886ea807ca49015ba31199e70b0e15d
Zisu Andrei-Costin <andrei-costin.zisu@daimler.com>
2021-04-08 18:07:31 +0200

    First version for devcontainers
    With support for mimicking the local user's id and group id, bash
    history persistence. Also it has support for SWF configuration for git.

---
+++ .devcontainer/post_start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+printf "\nSetting up .gitconfig and copying over GSEP keys\n"
+
+cp /host/.gitconfig $HOME/.gitconfig
+sed -i "s|$1|$HOME|g" $HOME/.gitconfig
+
+mkdir -p $HOME/.ssh
+cp -r /host/.ssh/gsep $HOME/.ssh
+
+printf "Setting up bash history persistency permissions\n"
+
+# We do a chown because VS Code will change the UID and GID to match the current user
+sudo chown -R $UID:$UID /commandhistory
+
+printf "\nAll done 👌\n\n"
+printf "👉 Build with something like:\n"
+
+printf "bazel build //bsw:amsr_vector_fs_socal_for_proxy --config=x86_64_linux && "
+printf "bazel build //bsw:amsr_vector_fs_socal_for_skeleton --config=x86_64_linux && "
+printf "bazel build //bsw:amsr_vector_fs_socal_for_software_update --config=x86_64_linux && "
+printf "bazel build //:minerva_mpu_adaptive_filesystem --config=x86_64_linux"
