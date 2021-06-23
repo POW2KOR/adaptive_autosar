248aab92ad5405687ea86658d7fee4b5a7330049
Fahad Umer <fahad.umer@daimler.com>
2021-06-23 18:51:23 +0200

    Add ssa client implementation in config manager

---
+++ application/x6aa_config_manager/src/diagnostic_ssa_client/diagnostic_ssa_client.cpp
@@ -0,0 +1,177 @@
+/*****************************************************************************
+ * \file   diagnostic_ssa_client.cpp
+ *
+ * \brief  SSA client implementation for receiving variant coding data
+ * \author MBition - Copyright (c) 2019-2020 Daimler AG
+ * \date
+ * \note   TODO: additional notes
+ ******************************************************************************/
+/* include own header first to ensure that is self contained */
+#include "diagnostic_ssa_client.h"
+
+/* C system includes */
+
+/* C++ system includes */
+#include <memory>
+#include <string>
+
+/* Project specific includes */
+
+using vac::container::literals::operator""_sv;
+
+namespace application {
+
+namespace ConfigManagerApp {
+
+std::atomic<bool> DiagnosticSsaClient::ssa_variant_coding_service_found{false};
+std::atomic<bool> DiagnosticSsaClient::ssa_variant_coding_events_subscribed{false};
+std::shared_ptr<
+    services::ns_si_dummyswc_2_to_cnfg_mngr::proxy::SI_X6AA_Dummy_B2_Service_ReservedProxy>
+    DiagnosticSsaClient::ssa_variant_coding_service_proxy;
+ara::core::InstanceSpecifier const DiagnosticSsaClient::ssa_variant_coding_instance_specifier{
+    "X6AA_Cnfg_Mngr_Executable/"
+    "X6AA_Cnfg_Mngr_ExecutableRootSwc/"
+    "AdaptiveRequiredPortType108"_sv};
+ara::log::Logger& DiagnosticSsaClient::GetLogger()
+{
+    static ara::log::Logger& logger{ara::log::CreateLogger("SSAP", "SSA Proxy")};
+    return logger;
+}
+
+DiagnosticSsaClient::DiagnosticSsaClient()
+{
+}
+
+DiagnosticSsaClient::~DiagnosticSsaClient()
+{
+}
+
+bool DiagnosticSsaClient::FindService()
+{
+    bool retval = true;
+    int counter = 0;
+    uint32_t max_try_count
+        = 10000; /* one while loop count is of nearly 10ms and timeout would be 100s (1000*10ms)*/
+
+    /* Start searching for variant coding services offered by SSA module */
+    GetLogger().LogInfo() << "Start searching for variant coding services offered by SSA module";
+    ara::com::FindServiceHandle find_service_handle
+        = services::ns_si_dummyswc_2_to_cnfg_mngr::proxy::SI_X6AA_Dummy_B2_Service_ReservedProxy::
+            StartFindService(FindServiceHandler, ssa_variant_coding_instance_specifier);
+
+    /* Wait until variant coding services offered by SSA module is found */
+    while (!ssa_variant_coding_service_found.load()) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        if (counter % 10 == 0) {
+            GetLogger().LogInfo()
+                << "Still searching for variant coding services offered by SSA module";
+        }
+        if (max_try_count == 0) {
+            GetLogger().LogFatal() << "Failed to find variant coding services offered by SSA module "
+                                     "-- timeout occured";
+            retval = false;
+            break;
+        }
+        counter++;
+        max_try_count--;
+    }
+
+    /* Stop searching for further services */
+    services::ns_si_dummyswc_2_to_cnfg_mngr::proxy::SI_X6AA_Dummy_B2_Service_ReservedProxy::
+        StopFindService(find_service_handle);
+    GetLogger().LogInfo() << "Stopped searching for variant coding services offered by SSA module";
+
+    return retval;
+}
+
+bool DiagnosticSsaClient::SubscribeToEvents()
+{
+    bool retval = false;
+
+    /* If a service was found */
+    if (ssa_variant_coding_service_found.load()) {
+        /* If the events have not been subscribed already */
+        if (!ssa_variant_coding_events_subscribed.load()) {
+            /* Subscribe to events and set handlers */
+            ssa_variant_coding_service_proxy->Ev_CalculationResult.SetReceiveHandler(
+                []() { EventHandlerForSsaVariantCodingData(); });
+            ssa_variant_coding_service_proxy->Ev_CalculationResult.Subscribe(
+                ara::com::EventCacheUpdatePolicy::kLastN, 1);
+
+            /* Remember that the events are subscribed */
+            ssa_variant_coding_events_subscribed.store(true);
+
+            retval = true;
+        }
+    }
+
+    return retval;
+}
+
+bool DiagnosticSsaClient::UnsubscribeFromEvents()
+{
+    bool retval = false;
+
+    /* If the events had been subscribed */
+    if (ssa_variant_coding_events_subscribed.load()) {
+        /* Unsubscribe from events */
+        ssa_variant_coding_service_proxy->Ev_CalculationResult.Unsubscribe();
+
+        /* Remember that the events are not subscribed */
+        ssa_variant_coding_events_subscribed.store(false);
+
+        retval = true;
+    }
+
+    return retval;
+}
+
+void DiagnosticSsaClient::FindServiceHandler(
+    ara::com::ServiceHandleContainer<services::ns_si_dummyswc_2_to_cnfg_mngr::proxy::
+                                         SI_X6AA_Dummy_B2_Service_ReservedProxy::HandleType>
+        ssa_variant_coding_services)
+{
+    /* If there was no service found */
+    if (ssa_variant_coding_services.size() == 0) {
+        GetLogger().LogInfo()
+            << "No instance of variant coding services offered by SSA module found";
+        return;
+    } else if (ssa_variant_coding_services.size() == 1) {
+        /* If there is exactly one service found */
+        GetLogger().LogInfo()
+            << "Found one instance of variant coding services offered by SSA module";
+    } else {
+        /* If there are multiple services found */
+        GetLogger().LogInfo() << "Found multiple instances of variant coding services offered by "
+                                "SSA module, use the first one";
+    }
+    /* Get proxy instance */
+    ssa_variant_coding_service_proxy = std::make_shared<
+        services::ns_si_dummyswc_2_to_cnfg_mngr::proxy::SI_X6AA_Dummy_B2_Service_ReservedProxy>(
+        ssa_variant_coding_services[0]);
+
+    /* Remember that a service is found */
+    ssa_variant_coding_service_found.store(true);
+}
+
+void DiagnosticSsaClient::EventHandlerForSsaVariantCodingData()
+{
+    GetLogger().LogInfo() << "EventHandlerForSsaVariantCodingData got called.";
+
+    /* If there are events available */
+    if (ssa_variant_coding_service_proxy->Ev_CalculationResult.Update()) {
+        auto samples = ssa_variant_coding_service_proxy->Ev_CalculationResult.GetCachedSamples();
+
+        GetLogger().LogInfo() << "Received " << samples.size() << " samples";
+
+        for (auto& sample : samples) {
+            GetLogger().LogInfo() << "Variant Coding: Received event with: "
+                                 << "Ev_CalculationResult        = '" << (*sample) << "' ";
+        }
+    }
+}
+
+} // namespace ConfigManagerApp
+
+} // namespace application
---
+++ application/x6aa_config_manager/src/diagnostic_ssa_client/diagnostic_ssa_client.h
@@ -0,0 +1,105 @@
+/*****************************************************************************
+ * \file   diagnostic_ssa_client.h
+ *
+ * \brief  SSA client implementation for receiving variant coding data
+ * \author MBition - Copyright (c) 2019-2020 Daimler AG
+ * \date
+ * \note   TODO: additional notes
+ ******************************************************************************/
+#ifndef SRC_DIAGNOSTIC_SSA_CLIENT_H_
+#define SRC_DIAGNOSTIC_SSA_CLIENT_H_
+
+/* C system includes */
+
+/* C++ system includes */
+#include <memory>
+
+/* Autosar includes */
+#include "ara/core/instance_specifier.h"
+#include "ara/log/logging.h"
+
+/* Project specific includes */
+#include "services/ns_si_dummyswc_2_to_cnfg_mngr/si_x6aa_dummy_b2_service_reserved_proxy.h"
+
+namespace application {
+
+namespace ConfigManagerApp {
+
+class DiagnosticSsaClient {
+public:
+    /*****************************************************************************
+     * \brief Constructor.
+     ******************************************************************************/
+    DiagnosticSsaClient();
+
+    /*****************************************************************************
+     * \brief Destructor.
+     ******************************************************************************/
+    virtual ~DiagnosticSsaClient();
+
+    /*****************************************************************************
+     * \brief Find SSA services for variant coding
+     *
+     * This method is blocking until the first service provider is discovered.
+     *
+     * \retval  true   Success
+     * \retval  false  Failure
+     ******************************************************************************/
+    static bool FindService();
+
+    /*****************************************************************************
+     * \brief Subscribe to service events and register handler for events.
+     *
+     * \retval  true   Success
+     * \retval  false  Failure
+     ******************************************************************************/
+    static bool SubscribeToEvents();
+
+    /*****************************************************************************
+     * \brief Unsubscribe from service events.
+     *
+     * \retval  true   Success
+     * \retval  false  Failure
+     ******************************************************************************/
+    static bool UnsubscribeFromEvents();
+
+private:
+    /*****************************************************************************
+     * \brief Handler for service discovery
+     *
+     * \param[in]  ssa_variant_coding_services  Container of found service handles.
+     ******************************************************************************/
+    static void FindServiceHandler(
+        ara::com::ServiceHandleContainer<services::ns_si_dummyswc_2_to_cnfg_mngr::proxy::
+                                             SI_X6AA_Dummy_B2_Service_ReservedProxy::HandleType>
+            ssa_variant_coding_services);
+
+    /*****************************************************************************
+     * \brief Handler for EventHandlerForSsaVariantCodingData event
+     ******************************************************************************/
+    static void EventHandlerForSsaVariantCodingData();
+
+    /**
+     * \brief Holds the logging context
+     */
+    static ara::log::Logger& GetLogger();
+
+    /** Flag that a service has been found */
+    static std::atomic<bool> ssa_variant_coding_service_found;
+
+    /** Flag that events have been subsribed */
+    static std::atomic<bool> ssa_variant_coding_events_subscribed;
+
+    /** Pointer to the bound service */
+    static std::shared_ptr<
+        services::ns_si_dummyswc_2_to_cnfg_mngr::proxy::SI_X6AA_Dummy_B2_Service_ReservedProxy>
+        ssa_variant_coding_service_proxy;
+    /** Instance specifier for the service */
+    static ara::core::InstanceSpecifier const ssa_variant_coding_instance_specifier;
+};
+
+} // namespace ConfigManagerApp
+
+} // namespace application
+
+#endif // SRC_DIAGNOSTIC_SSA_CLIENT_H_
