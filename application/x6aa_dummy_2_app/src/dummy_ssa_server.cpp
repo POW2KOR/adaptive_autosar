910ad9ecc9c28c21aeaa0d3df92f047993e82d21
Fahad Umer <fahad.umer@daimler.com>
2021-06-28 12:33:53 +0200

    Use application base parent class
    - there is a runtime issue while using application base class, during runtime exit_code=6 is returned

--- application/x6aa_dummy_2_app/src/dummy_ssa_server.cpp
+++ application/x6aa_dummy_2_app/src/dummy_ssa_server.cpp
@@ -12,8 +12,6 @@
  *********************************************************************************************************************/
 #include "dummy_ssa_server.h"
 
-#include "dummy_ssa_server_error_domain.h"
-
 namespace application {
 
 #define BODY_STYLES_COUNT 10
@@ -24,14 +22,9 @@ using vac::container::operator""_sv;
 /*!
  * \brief Initialize DummySsaServer application.
  */
-DummySsaServer::DummySsaServer()
+DummySsaServer::DummySsaServer() : ApplicationBase("dummySsa")
 {
     log_.LogInfo() << "Dummy SSA server is initializing...";
-
-    this->StartSignalHandlerThread().InspectError([this](ara::core::ErrorCode const& error) {
-        has_initialization_failed_ = true;
-        log_.LogFatal() << error.Message() << ". " << error.UserMessage();
-    });
 
     // Set up the dummySsaServer sending data to config manager
     ara::core::InstanceSpecifier const dummySsaServerInstanceSpecifier{
@@ -51,70 +44,9 @@ DummySsaServer::DummySsaServer()
  */
 DummySsaServer::~DummySsaServer()
 {
-    exit_requested = true;
-
     // deinitialze the dummySsaServer
     dummySsaServer->StopOfferService();
     dummySsaServer.reset();
-
-    log_.LogInfo() << "Dummy SSA server shutdown initiated.";
-
-    if (signal_handler_thread.native_handle() != 0) {
-        /* #10 Check if exit was requested by sending SIGTERM or SIGINT. */
-        if (!terminated_by_signal) {
-            /* #15 Terminate the signal handler thread to shutdown. */
-            if (0
-                != pthread_kill(
-                    static_cast<pthread_t>(signal_handler_thread.native_handle()), SIGTERM)) {
-                log_.LogError() << "Invalid signal!";
-            }
-        } else {
-            log_.LogDebug() << "SIGINT or SIGTERM had been received and had been handled";
-        }
-    } else {
-        log_.LogError() << "Thread ID = 0";
-    }
-
-    /* #20 Wait till all threads have joined. */
-    if (signal_handler_thread.joinable()) {
-        signal_handler_thread.join();
-    }
-
-    log_.LogInfo() << "Dummy SSA server finished shutdown.";
-}
-
-/*!
- * \brief Start a thread handling all signals send to this process.
- * \internal
- * - #10 Create ara::core::Result object.
- * - #20 Create thread_name.
- * - #30 Start signal handler thread.
- * - #40 Create the thread native_handle object.
- * - #50 Set thread name and return the ara::core::Result object.
- * \endinternal
- */
-ara::core::Result<osabstraction::process::ProcessId> DummySsaServer::StartSignalHandlerThread()
-{
-    /* #10 Create ara::core::Result object. */
-    using R = ara::core::Result<osabstraction::process::ProcessId>;
-
-    /* #20 Create thread_name. */
-    vac::container::CStringView thread_name
-        = vac::container::CStringView::FromLiteral("SkeletonSignal", 10);
-
-    /* #30 Start signal handler thread. */
-    signal_handler_thread = std::thread(&DummySsaServer::SignalHandlerThread, this);
-
-    /* #40 Create the thread native_handle object. */
-    std::thread::native_handle_type const thread_id{(signal_handler_thread).native_handle()};
-
-    /* #50 Set thread name and return the ara::core::Result object. */
-    return osabstraction::thread::SetNameOfThread(thread_id, thread_name)
-        .AndThen([]() -> R { return R{osabstraction::process::GetProcessId()}; })
-        .OrElse([thread_name, this](ara::core::ErrorCode) -> R {
-            log_.LogFatal() << "Naming of thread '" << thread_name << "' failed";
-            return R::FromError(DummySsaServerErrc::kThreadCreationFailed, "Naming failed.");
-        });
 }
 
 std::int8_t DummySsaServer::Run()
@@ -127,7 +59,7 @@ std::int8_t DummySsaServer::Run()
 
         log_.LogInfo() << "Dummy SSA server started";
 
-        while (!exit_requested) {
+        while (!exit_requested_) {
 
             std::this_thread::sleep_for(std::chrono::seconds(5));
             dummySsaServer->Ev_CalculationResult.Send(bodyStyleValues[count % BODY_STYLES_COUNT]);
@@ -143,119 +75,6 @@ std::int8_t DummySsaServer::Run()
     this->ReportApplicationState(ara::exec::ApplicationState::kTerminating);
 
     return ret;
-}
-
-/*!
- * \internal
- * - #10 empty the set of signals.
- * - #20 add SIGTERM to signal set.
- * - #21 add SIGINT to signal set.
- * - #30 loop until SIGTERM or SIGINT signal received.
- * - #35 request application exit.
- * \endinternal
- */
-void DummySsaServer::SignalHandlerThread()
-{
-    sigset_t signal_set;
-
-    /* #10 empty the set of signals. */
-    if (0 != sigemptyset(&signal_set)) {
-        log_.LogFatal([](ara::log::LogStream& s) {
-            s << "Dummy SSA server could not empty signal "
-                 "set.";
-        });
-        ara::core::Abort("Empty signal set failed.");
-    }
-    /* #20 add SIGTERM to signal set. */
-    if (0 != sigaddset(&signal_set, SIGTERM)) {
-        log_.LogFatal([](ara::log::LogStream& s) {
-            s << "Dummy SSA server cannot add signal to "
-                 "signalset: SIGTERM";
-        });
-        ara::core::Abort("Adding SIGTERM failed.");
-    }
-    /* #21 add SIGINT to signal set. */
-    if (0 != sigaddset(&signal_set, SIGINT)) {
-        log_.LogFatal([](ara::log::LogStream& s) {
-            s << "Dummy SSA server cannot add signal to "
-                 "signalset: SIGINT";
-        });
-        ara::core::Abort("Adding SIGINT failed.");
-    }
-
-    /* #30 loop until SIGTERM or SIGINT signal received. */
-    int sig{-1};
-
-    do {
-        if (0 != sigwait(&signal_set, &sig)) {
-            log_.LogFatal([](ara::log::LogStream& s) {
-                s << "Dummy SSA server called sigwait() "
-                     "with invalid signalset";
-            });
-            ara::core::Abort("Waiting for SIGTERM or SIGINT failed.");
-        }
-        log_.LogInfo([&sig](ara::log::LogStream& s) {
-            s << "Dummy SSA server received signal: " << sig << ".";
-        });
-
-        if ((sig == SIGTERM) || (sig == SIGINT)) {
-            log_.LogInfo([](ara::log::LogStream& s) {
-                s << "Dummy SSA server received SIGTERM "
-                     "or SIGINT, requesting application shutdown.";
-            });
-            if (!exit_requested) {
-                /* #35 request application exit. (SignalHandler initiate the shutdown!) */
-                exit_requested = true;
-            }
-            terminated_by_signal = true;
-        }
-    } while (!exit_requested);
-}
-
-/*!
- * \internal
- * - #10 verify which state should be reported
- * -    #15 invalid application state detected
- * - #20 check if invalid application state was detected.
- * - #30 send application state
- * -    #35 application state could not be set.
- * \endinternal
- */
-void DummySsaServer::ReportApplicationState(ara::exec::ApplicationState application_state)
-{
-    std::string application_state_string = "";
-    bool error_occurred = false;
-
-    /* #10 verify which state should be reported  */
-    if (application_state == ara::exec::ApplicationState::kRunning) {
-        application_state_string = "kRunning";
-    } else if (application_state == ara::exec::ApplicationState::kTerminating) {
-        application_state_string = "kTerminating";
-    } else {
-        /* #15 invalid application state detected */
-        error_occurred = true;
-        log_.LogError() << "ReportApplicationState called with an invalid application state: ";
-    }
-
-    /* #20 check if invalid application state was detected. */
-    if (!error_occurred) {
-        log_.LogDebug() << "Dummy SSA server is reporting "
-                           "Application state "
-                        << application_state_string;
-
-        /* #30 send application state */
-        if (application_client.ReportApplicationState(application_state)
-            == ara::exec::ApplicationReturnType::kSuccess) {
-            log_.LogDebug() << "Dummy SSA server reported "
-                               "Application state "
-                            << application_state_string << " successfully";
-        } else {
-            /* #35 application state could not be set. */
-            log_.LogError() << "Dummy SSA server could "
-                               "not report the Application state "
-                            << application_state_string;
-        }
-    }
 }
 
 } // namespace application
--- application/x6aa_dummy_2_app/src/dummy_ssa_server.h
+++ application/x6aa_dummy_2_app/src/dummy_ssa_server.h
@@ -13,18 +13,8 @@
 /**********************************************************************************************************************
  *  INCLUDES
  *********************************************************************************************************************/
-#include "ara/core/abort.h"
-#include "ara/core/error_code.h"
-#include "ara/core/error_domain.h"
-#include "ara/core/exception.h"
-#include "ara/core/initialization.h"
-#include "ara/exec/application_client.h"
-#include "ara/log/logging.h"
-#include "osabstraction/process/process.h"
-#include "osabstraction/thread/thread.h"
 #include "services/ns_si_dummyswc_2_to_cnfg_mngr/si_x6aa_dummy_b2_service_reserved_skeleton.h"
-#include "vac/container/string_literals.h"
-#include "vac/language/throw_or_terminate.h"
+#include "application/common/application_base.h"
 
 #include <csignal>
 #include <iostream>
@@ -39,7 +29,7 @@ namespace application {
  * \brief Main class representing the DummySsaServer.
  * \vprivate Example class for component internal use.
  */
-class DummySsaServer final {
+class DummySsaServer final : public ApplicationBase {
 public:
     /*!
      * \brief Constructor of class DummySsaServer.
@@ -80,55 +70,6 @@ public:
     std::int8_t Run();
 
 private:
-    /*!
-     * \brief Start threads given by information in handed over thread information.
-     * \param thread_info The thread information array.
-     * \return The process id of the started thread.
-     * \error TemplateErrc::kThreadCreationFailed Naming the thread after creation has failed.
-     */
-    ara::core::Result<osabstraction::process::ProcessId> StartSignalHandlerThread();
-
-    /*!
-     * \brief Entry point of the thread receiving signals from the execution manager.
-     */
-    void SignalHandlerThread();
-
-    /*!
-     * \brief Reporting the given application state
-     * \param application_state The application state to report
-     */
-    void ReportApplicationState(ara::exec::ApplicationState application_state);
-
-    /*!
-     * \brief Logger instance.
-     */
-    ara::log::Logger& log_{ara::log::CreateLogger("APP", "Dummy SSA Server")};
-
-    /*!
-     * \brief ApplicationClient instance
-     */
-    ara::exec::ApplicationClient application_client;
-
-    /*!
-     * \brief Signal handler thread.
-     */
-    std::thread signal_handler_thread{};
-
-    /*!
-     * \brief Flag to exit the application.
-     */
-    std::atomic_bool exit_requested{false};
-
-    /*!
-     * \brief Flag to mark if the application exit was triggered by a signal.
-     */
-    std::atomic_bool terminated_by_signal{false};
-
-    /*!
-     * \brief Flag to indicate initialization error. Only used in main thread.
-     */
-    bool has_initialization_failed_{false};
-
     /*!
      * \brief SI_X6AA_Dummy_B2_Service_ReservedSkeleton
      */
