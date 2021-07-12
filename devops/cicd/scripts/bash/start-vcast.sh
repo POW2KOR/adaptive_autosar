d2360048abca223c630640a54e2735d31a4aa39b
Madhu kiran Cherlopalli <madhu_kiran.cherlopalli@daimler.com>
2021-07-12 15:25:06 +0200

    Fix review comments of code
    Add args of path variables in docker file

--- devops/cicd/scripts/bash/start-vcast.sh
+++ devops/cicd/scripts/bash/start-vcast.sh
@@ -2,7 +2,7 @@
 # Usage:unencrypt.sh<passphrase><release_archive>
 
 
-#Update the .lic file to the latest version and the location where it will get downloaded
+#TODO : Update the .lic file to the latest version and the location where it will get downloaded
 cd /usr/tools/vcast/
 ./flexlm/lmgrd -c ./vector-IP-10-101-1-166.lic -l logs.txt
 sleep 5s
